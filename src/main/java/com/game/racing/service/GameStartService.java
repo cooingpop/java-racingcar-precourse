b7e5463e4679a63b1844893c1bca81b828b31c39
cooingpop <cooingpop@gmail.com>
2021-05-02 22:50:17 +0900

    feat: 자동차 게임 메인실행 추가

---
+++ src/main/java/com/game/racing/service/GameStartService.java
@@ -0,0 +1,28 @@
+/*
+ * @(#) GameStartService.java 2021. 05. 02.
+ *
+ * Created by cooingpop
+ */
+
+package com.game.racing.service;
+
+import java.util.Scanner;
+
+/**
+ * @author 박준영
+ **/
+public class GameStartService {
+	private static final String GAME_START_MESSAGE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
+	private static final String GAME_TRY_MESSAGE = "시도할 회수는 몇회인가요?";
+	private static final String GAME_PLAY_MESSAGE = "실행 결과";
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println(GAME_START_MESSAGE);
+		String input = scanner.next();
+
+		System.out.println(GAME_TRY_MESSAGE);
+		System.out.println(GAME_PLAY_MESSAGE);
+	}
+}
