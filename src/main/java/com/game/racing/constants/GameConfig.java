fcc2d9feced25980fc33677961a22ebcc556b4ce
cooingpop <cooingpop@gmail.com>
2021-05-03 22:19:48 +0900

    feat: 게임에 필요한 상수값 추가
    랜덤 최대값, 최소값, 움직일 수 있는 최소값, 프로그레스바

---
+++ src/main/java/com/game/racing/constants/GameConfig.java
@@ -0,0 +1,17 @@
+/*
+ * @(#) GameConfig.java 2021. 05. 03.
+ *
+ * Created by cooingpop
+ */
+
+package com.game.racing.constants;
+
+/**
+ * @author 박준영
+ **/
+public class GameConfig {
+	public static final int RANDOM_MAX_VALUE = 9;
+	public static final int RANDOM_MIN_VALUE = 1;
+	public static final int GO_FORWARD_AVAILABLE_MIN_VALUE = 4;
+	public static final String MOVE_PROGRESS_BAR = "-";
+}
