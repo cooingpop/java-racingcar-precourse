5b83246041866bf7ca35a6d230a51a8a001d2371
cooingpop <cooingpop@gmail.com>
2021-05-03 22:22:33 +0900

    feat: 자동차 2대 이상인 경우 실행 가능여부 확인, 우승자 선별 기능추가

--- src/main/java/com/game/racing/model/GamePlayer.java
+++ src/main/java/com/game/racing/model/GamePlayer.java
@@ -13,34 +13,95 @@ import java.util.List;
  * @author 박준영
  **/
 public class GamePlayer {
+	private final int MOVE_MAX_RANGE = 9;
+	private final int MOVE_MIN_RANGE = 0;
+
 	private List<Car> carList;
 	private int moveCount;
+	private int tryCount;
 
 	public GamePlayer(String input) {
-		carList = new ArrayList<>();
+		this.carList = new ArrayList<>();
 		String[] playerName = input.trim().split(",");
 
 		for (String name : playerName) {
-			carList.add(new Car(name));
+			this.carList.add(new Car(name));
 		}
 	}
 
 	public List<Car> getCarList() {
-		return carList;
+		return this.carList;
 	}
 
 	public int getPlayerCount() {
 		return getCarList().size();
 	}
 
-	public void setMoveCount(int moveCount) {
-		this.moveCount = moveCount;
+	public void setTryCount(int tryCount) {
+		this.tryCount = tryCount;
 	}
 
 	public int getMoveCount() {
-		return moveCount;
+		return this.moveCount;
 	}
 
-	public void start() {
+	public int getTryCount() {
+		return this.tryCount;
 	}
+
+	public boolean isAvailableRacing() {
+		if (getCarList().size() > 1) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public boolean isGameCompleted() {
+		if (this.moveCount == this.tryCount) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public void race() {
+		for (Car car : this.carList) {
+			car.goForward();
+		}
+
+		this.moveCount++;
+	}
+
+	public int getWinnerPosition() {
+		int maxValue = 0;
+
+		for (Car car : this.carList) {
+			maxValue = Math.max(car.getPosition(), maxValue);
+		}
+
+		return maxValue;
+	}
+
+	public List<Car> getWinners(List<Car> carList) {
+		List<Car> winners = new ArrayList<>();
+		int maxValue = getWinnerPosition();
+		for (Car car : carList) {
+			if (maxValue == car.getPosition()) {
+				winners.add(car);
+			}
+		}
+
+		return winners;
+	}
+
+
+	public void printGameResult() {
+		for (Car car : this.carList) {
+			System.out.println(String.format("%s : %s", car.getName(), car.getCurrentPositionResult()));
+		}
+		System.out.println();
+	}
+
+
 }
