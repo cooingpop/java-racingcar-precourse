12032e86a77580edf787962c45e781471cf26c30
cooingpop <cooingpop@gmail.com>
2021-05-04 19:46:15 +0900

    fix: 랜덤 범위 최소값 수정

--- src/main/java/com/game/racing/constants/GameConfig.java
+++ src/main/java/com/game/racing/constants/GameConfig.java
@@ -11,7 +11,7 @@ package com.game.racing.constants;
  **/
 public class GameConfig {
 	public static final int RANDOM_MAX_VALUE = 9;
-	public static final int RANDOM_MIN_VALUE = 1;
+	public static final int RANDOM_MIN_VALUE = 0;
 	public static final int GO_FORWARD_AVAILABLE_MIN_VALUE = 4;
 	public static final String MOVE_PROGRESS_BAR = "-";
 }
